a92fa771a0bd1c675384841a18f2558ed43f6078
jeonjunhyeon <friendwnsgus@naver.com>
2021-04-13 20:15:23 +0900

    test33

--- project_talktalk/src/common/aa.java
+++ project_talktalk/src/common/aa.java
@@ -5,27 +5,15 @@ public class aa {
 		System.out.println("sdfsdfsdfsdf");
 	}
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("10시 퇴근");
-=======
-<<<<<<< HEAD
 		// TODO Auto-generated method stub
 			System.out.println(" 무야호 ");
 			System.out.println("호우호우");
-=======
 		System.out.println("안녕하세요");
 		new aa().a();
-<<<<<<< HEAD
 		System.out.println("내가 만들었더가싀");
 		System.out.println("내가 만들었더가싀");
-=======
-<<<<<<< HEAD
 		System.out.println("안녕히가세요");
-=======
->>>>>>> refs/heads/main
->>>>>>> branch 'main' of https://github.com/goodlucky1215/project_talktalk.git
->>>>>>> refs/heads/main
->>>>>>> refs/heads/main
 	}
 
 }
