c509cc7a03665ee893315d303938549f6ef75f0d
goodlucky1215 <63006616+goodlucky1215@users.noreply.github.com>
2021-04-19 18:50:00 +0900

    Delete project_talktalk/src/common directory

--- project_talktalk/src/common/IUisIU.java
+++
@@ -1,8 +0,0 @@
-package common;
-
-public class IUisIU {
-
-	public static void main(String[] args) {
-		System.out.println("아이유는 누구집 아이유?");
-	}
-}
--- project_talktalk/src/common/abc.java
+++
@@ -1,9 +0,0 @@
-package common;
-
-public class abc {
-
-	public static void main(String[] args) {
-		System.out.println("Git Fighting");
-	}
-
-}
